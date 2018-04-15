301225f1453571cbe45c8c868a467f38760cb911
Alvin Lim <alvink77@gmail.com>
2018-04-15 19:58:35 +0700

    adde dtrimacche but not working

--- sw.js
+++ sw.js
@@ -53,13 +53,9 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
     console.log('[Service Worker] Fetching data...')
 
-    if (isInArray(event.request.url, STATIC_CACHE)) {
-        console.log('[Service Worker] Fetch from STATIC_CACHE')
+    if (isInArray(event.request.url, STATIC_CACHE))
         event.respondWith(caches.match(event.request))
-    } else {
-        console.log('[Service Worker] Fetch Dynamic and Cache to dynamic')
-        event.respondWith(cacheAndFetchStrategy(event))
-    }
+    else event.respondWith(cacheAndFetchStrategy(event))
 })
 
 /* End of Service Worker Events */
@@ -81,15 +77,22 @@ var cacheOnlyThenNetwork = function(event) {
     })
 }
 
+var firebaseURL =
+    'firebasestorage.googleapis.com/v0/b/final-essay-dev.appspot.com/'
+
 var cacheAndFetchStrategy = function(event) {
     return caches.open(DYNAMIC_VERSION).then(function(cache) {
         return cache.match(event.request).then(function(response) {
-            const { url } = event.request
-            if (~url.indexOf('auth/google')) return res
+            var { url } = event.request
+            if (~url.indexOf('auth/google')) return response
 
             var fetchPromise = fetch(event.request).then(function(
                 networkResponse
             ) {
+                if (~url.indexOf(firebaseURL)) {
+                    // Should trim image for more than 10
+                    // trimCache(DYNAMIC_VERSION, firebaseURL, 1)
+                }
                 cache.put(event.request, networkResponse.clone())
                 return networkResponse
             })
@@ -108,4 +111,44 @@ var isInArray = function(string, array) {
     } else cachePath = string // store the full request (for CDNs)
 
     return array.indexOf(cachePath) > -1
+}
+
+// Continue with the logic of trimming the cache
+var trimCache = function(cacheName, url, totalToTrim, indexToPop) {
+    caches.open(cacheName).then(cache => {
+        return cache.keys().then(keys => {
+            console.log(indexToPop, keys.length)
+            if (!indexToPop) {
+                var arr = []
+                var keysURL = keys.map(function(k) {
+                    return k.url
+                })
+
+                var indexKey = keysURL.findIndex(function(name) {
+                    return ~name.indexOf(firebaseURL)
+                })
+
+                if (indexKey === -1) return
+
+                while (indexKey !== -1 || arr.length === totalToTrim) {
+                    console.log(keysURL.splice(indexKey, 1))
+                    arr.push(indexKey)
+
+                    indexKey = keysURL.findIndex(function(name) {
+                        return ~name.indexOf(firebaseURL)
+                    })
+                }
+
+                return trimCache(cacheName, url, totalToTrim, arr)
+            } else if (indexToPop.length === 0) return
+            else
+                return cache.delete(indexToPop.splice(0, 1)).then(function() {
+                    console.log(
+                        '[Service Worker] Continue Trimming',
+                        indexToPop
+                    )
+                    trimCache(cacheName, url, totalToTrim, indexToPop)
+                })
+        })
+    })
 }

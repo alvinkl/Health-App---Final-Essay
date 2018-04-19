9d55c4389537688759f4e725b47898161fd845e2
Alvin Lim <alvink77@gmail.com>
2018-04-19 12:00:29 +0700

    added keys for production using process env

--- src/server/config/keys.js
+++ src/server/config/keys.js
@@ -1,32 +1,20 @@
-module.exports = {
-    cookieKey: 'cookieKeycookie',
-    // DB URI
-    mongoURI: 'mongodb://localhost/healthapp',
-
-    // GOOGLE AUTH
-    GoogleClientID:
-        '415704818384-for77vqcd1bf73pfv8a56tmf3s5grg98.apps.googleusercontent.com',
-    GoogleClientSecret: 'c2vPSelfPQTjbF3DRUfmqJIJ',
-
-    // FOOD API
-    NutritionXAppID: '559e441c',
-    NutritionXAppKeys: 'b5372722af0247ff5cda75e3fdf36c2c',
-
-    // Google Maps
-    GoogleAPIKey: 'AIzaSyAqc0Dmk-FuQDD4irHprMg7koo8TBpfZcg',
-
-    // Google Geocoding Maps
-    GoogleGeocodingAPIKey: 'AIzaSyC1j9Y4f72dto_M6JEeaK-Vo4wsc0d1xt8',
-
-    ZomatoAPIKey: 'a0fd49f9dd95817830343343a8039a4f',
-
-    // Web Push Notification
-    VapidPrivateKeys: '6e-PR2S_emWv4bd2Ja0J1X1xYEwleuqwPlA3yALGNDQ',
-    VapidPublicKeys:
-        'BP58tstnkFgp6q0S75xUyODy6SnJyG5RDP_Ku_4A8IVt22nYctFSmtUnDw_xo-4sepu3y0XM2IvMLaZmQ1qES3c',
-
-    // Credentials
-    credentials: {
-        email: 'mailto:alvink77@gmail.com',
-    },
+if (process.env.NODE_ENV === 'development') {
+    module.exports = require('./keys-development').default
+} else {
+    module.exports = {
+        cookieKey: process.env.COOKIE_KEY,
+        mongoURI: process.env.MONGO_URI,
+        GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+        GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        NutritionXAppID: process.env.NUTRITIONX_APP_ID,
+        NutritionXAppKeys: process.env.NUTRITIONX_APP_KEYS,
+        GoogleAPIKey: process.env.GOOGLE_API_KEY,
+        GoogleGeocodingAPIKey: process.env.GOOGLE_GEOCODING_API_KEY,
+        ZomatoAPIKey: process.env.ZOMATO_API_KEY,
+        VapidPrivateKeys: process.env.VAPID_PRIVATE_KEYS,
+        VapidPublicKeys: process.env.VAPID_PUBLIC_KEYS,
+        credentials: {
+            email: process.env.CREDENTIALS_EMAIL,
+        },
+    }
 }

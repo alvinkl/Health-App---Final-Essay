69e72f0cdb90c48020529d2c974c6f32211bfc18
Alvin Lim <alvink77@gmail.com>
2018-04-20 19:27:59 +0700

    added datatseed for AW

--- dataseed/insertToDB.js
+++ dataseed/insertToDB.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const m = require('mongoose')
-const { mongoURI } = require('../src/server/config/keys')
+const mongoURI = 'mongodb://localhost/healthapp'
 
 m
     .connect(mongoURI)
